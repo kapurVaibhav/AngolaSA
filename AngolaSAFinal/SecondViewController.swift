b99f789c3628a2513c898efa4f8001288d696ad5
Vaibhav Kapur <vaibhavkapur@vaibhavs-macbook-pro.local>
2016-10-18 11:05:19 +0530

    Initial Commit

---
+++ AngolaSAFinal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AngolaSAFinal
+//
+//  Created by Vaibhav Kapur on 18/10/2016.
+//  Copyright © 2016 AgeAnalytics. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
